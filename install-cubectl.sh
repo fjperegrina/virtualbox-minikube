dca87740a4b7374a69edbca9eea152ae2b1ff577
fjperegrina <franciscojavier.peregrinamartinez@altran.com>
2018-08-01 09:59:19 +0200

    scripts minikube kubectl

---
+++ install-cubectl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.0/bin/darwin/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+
---
+++ install-minikube.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+
